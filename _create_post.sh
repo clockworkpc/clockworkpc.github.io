6f8ad58581ee62d084a2777e039ecba63c8ddc56
alexander <agarber@profectusgroup.com>
2020-03-21 17:39:28 +1100

    create_post

--- _create_post.sh
+++ _create_post.sh
@@ -6,6 +6,7 @@ dateStamp=`date +"%Y"-%m-%d-`
 myTitle=$1
 myTags=$2
 
+# Generate YAML front matter by sub-string replacement
 myYml="${ymlTemplate/MY_TITLE/$myTitle}"
 myYml="${myYml/MY_DATE/$currentDate}"
 myYml="${myYml/MY_TAGS/$myTags}"
@@ -15,6 +16,14 @@ myBasename=`echo $myBasename | sed 's/[^a-zA-Z0-9]/_/g' | sed -e 's/\([_]\)\1\+/
 
 filePath="`pwd`/_posts/${dateStamp}${myBasename}.md"
 
-echo "$myYml" > $filePath
-nvim "$filePath"
+if [[ -z "$myTitle" ]] ; then
+  echo "No title provided"
+elif [[ -z "$myTags" ]] ; then
+  echo "No tags provided"
+else
+  echo "${myYml}" > $filePath
+  echo "# ${myTitle}" >> ${filePath}
+  nvim "${filePath}"
+  glow "${filePath}"
+fi
 
---
+++ _posts/2020-03-21-hello_world.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  Hello, World
+date:   Sat 21 Mar 17:38:08 AEDT 2020
+tags: [test, tag]
+---
+# Hello, World
+
+This is something.
